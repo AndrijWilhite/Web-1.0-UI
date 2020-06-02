0097118241a57a2a2dc724740ef65b425c650c8a
Andrij Wilhite <andrij.wilhite@gmail.com>
2020-06-01 18:39:14 -0700

    clean up of userCard.js

--- src/components/userCard/userCard.js
+++ src/components/userCard/userCard.js
@@ -1,43 +1,3 @@
-// const template = document.createElement('template')
-// template.innerHTML = `
-// <style>
-//   .user-card {
-//         font-family: 'Arial', sans-serif;
-//         background: #f4f4f4;
-//         width: 500px;
-//         display: grid;
-//         grid-template-columns: 1fr 2fr;
-//         grid-gap: 10px;
-//         margin-bottom: 15px;
-//         border-bottom: darkorchid 5px solid;
-//     }
-
-//     .user-card img {
-//         width: 100%;
-//     }
-
-//     .user-card button {
-//         cursor: pointer;
-//         background: darkorchid;
-//         color: #fff;
-//         border: 0;
-//         border-radius: 5px;
-//         padding: 5px 10px;
-//     }
-//   </style>
-//   <div class="user-card">
-//     <img />
-//     <div>
-//       <h3></h3>
-//       <div class="info">
-//         <p><slot name="email"/></p>
-//         <p><slot name="phone" /></p>
-//       </div>
-//       <button id="toggle-info">Hide Info</button>
-//     </div>
-//   </div>
-// `
-
 import template from './template.js'
 
 export class UserCard extends HTMLElement {
@@ -76,5 +36,3 @@ export class UserCard extends HTMLElement {
     this.shadowRoot.querySelector('#toggle-info').removeEventListener()
   }
 }
-
-// window.customElements.define('user-card', UserCard)
