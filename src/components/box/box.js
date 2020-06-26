f60e60ece84ca552aa78f9c55cf067d23bf7f9b8
Andrij Wilhite <andrij.wilhite@gmail.com>
2020-06-26 15:29:03 -0700

    added h and w params to box

--- src/components/box/box.js
+++ src/components/box/box.js
@@ -1,7 +1,9 @@
 import template from './template.js'
 
 /**
- * @param scroll : boolean
+ * @param scroll : boolean, enables scroll bar if true
+ * @param height : height you want the scroll box to be
+ * @param width : width you want the scroll box to be
  */
 
 export class Box extends HTMLElement {
@@ -10,6 +12,9 @@ export class Box extends HTMLElement {
 
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this.shadowRoot.getElementById('box').innerHTML = this.innerHTML
+
+    this.shadowRoot.querySelector('style').innerText += '#box {width:' + (this.getAttribute('width') || '300') + 'px;height:' + (this.getAttribute('height') || '500') + 'px;}'
 
     if (this.getAttribute('scrollable') === 'true') {
       let height = +getComputedStyle((this.shadowRoot.getElementById('box'))).borderTopWidth.slice(0, -2) * 2 + this.shadowRoot.getElementById('box').clientHeight
--- src/components/box/template.js
+++ src/components/box/template.js
@@ -1,30 +1,9 @@
 const template = document.createElement('template')
-let lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempor eros sit amet augue efficitur tristique. Praesent lacinia lectus ut urna accumsan elementum. Cras fermentum nunc ac facilisis sagittis. Vestibulum lobortis in lorem in malesuada. Pellentesque id orci sodales, eleifend ipsum sit amet, pretium dui. Nullam sagittis ipsum eu malesuada gravida. Sed rhoncus metus ac ante ullamcorper dictum. Donec ullamcorper sagittis massa, sed lobortis neque vulputate finibus. Nunc ornare neque eget eros aliquet tempus. Donec mattis consectetur sem, a vestibulum odio lobortis ut. Cras in sapien ut sem hendrerit facilisis.
-
-Nulla mi ligula, varius quis tristique vel, mollis ac leo. Sed blandit nibh magna, vulputate porta tellus mollis laoreet. Vivamus id erat egestas, finibus felis pulvinar, suscipit elit. Sed sit amet congue ipsum, vel scelerisque ante. Sed vitae turpis sed velit volutpat sodales. Nulla lobortis dui hendrerit, semper felis et, lobortis nisl. Aliquam tempus augue tortor, eu pulvinar magna tincidunt eget. Vestibulum vel convallis metus. Curabitur nec fringilla metus, vitae luctus purus.
-
-Praesent id orci urna. Morbi nisi sapien, semper at nulla quis, elementum mattis libero. In lacinia dui sed nulla fermentum convallis. Ut dui odio, maximus feugiat dignissim in, placerat eu lorem. Proin aliquam odio ut risus semper consectetur. Integer et consectetur tortor, vitae mollis eros. Nulla non fringilla tortor. Integer ornare arcu vitae sodales pretium. Praesent et accumsan justo, lacinia sodales orci. Sed consectetur non elit sed gravida. Cras pretium ullamcorper vehicula. Phasellus vestibulum erat quam, sit amet consequat urna efficitur at. Aliquam rutrum tortor ex, vitae sodales magna maximus id. Cras dignissim mauris luctus nisi pellentesque consequat. Quisque pellentesque tellus ac lectus porta porta.
-
-Integer tincidunt venenatis augue, vitae pharetra libero sagittis vitae. Nullam ultrices faucibus dui, vestibulum auctor lectus fermentum vitae. Donec ex ex, aliquam in urna at, lacinia posuere nulla. Curabitur non risus venenatis, pretium nisi at, vulputate nisl. Curabitur tincidunt venenatis arcu eu fringilla. Nullam mollis ac nisi eget tempus. Pellentesque at erat congue tortor aliquam luctus eget id sem. Ut lacinia tristique sollicitudin. Aenean pellentesque consequat placerat. Curabitur tempus tortor sit amet neque ornare accumsan bibendum vitae ipsum. Integer non tempor sapien. Proin orci lectus, hendrerit varius ornare quis, tempus a libero. Proin vel bibendum est, aliquet congue ex.
-
-Cras tristique vehicula turpis eu molestie. Praesent at metus sit amet magna pharetra luctus vitae non ante. Duis vestibulum maximus lacus eget placerat. Aliquam venenatis odio nec nulla iaculis, nec hendrerit eros auctor. Morbi id tellus tincidunt, posuere ante id, mollis enim. Ut volutpat volutpat enim non efficitur. Donec vitae nunc vulputate, laoreet nulla ut, finibus elit. Donec sodales vulputate rhoncus. Sed elit mi, posuere vel pellentesque sed, posuere ac diam. Sed ac libero lectus. Duis eleifend, tortor ut mattis pharetra, justo urna luctus risus, maximus posuere orci neque nec est. Quisque eu aliquet dolor. Morbi vel imperdiet tellus, vitae vestibulum odio. Sed vestibulum, massa non scelerisque sagittis, metus lorem dignissim elit, eu porta magna nisi eu ex. Duis tempor pharetra erat, ac vehicula urna.
-
-Integer nec auctor ante. Proin vel urna mattis, vulputate metus non, pretium ex. Sed imperdiet risus ante, sit amet posuere nulla molestie pulvinar. Mauris ut maximus metus. Proin euismod, risus nec dapibus gravida, sem quam sodales ex, quis porta orci metus eu massa. Vestibulum nisi lectus, pretium sit amet lorem eu, eleifend pellentesque ex. In molestie tempus bibendum. Fusce feugiat auctor nulla sit amet sodales. Integer tincidunt ornare ligula, ac cursus quam commodo ac. Donec vulputate tellus nec efficitur mattis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin scelerisque ornare eros, at ornare nulla tempor eu.
-
-Maecenas in iaculis sapien, eget faucibus neque. Aenean semper sodales dui, eget aliquet metus cursus ac. Etiam laoreet est non felis ornare gravida. Aenean justo mi, aliquam eu consectetur non, malesuada vitae urna. Sed ut bibendum diam. Donec tristique eros eu lorem imperdiet varius. Nam tempus eu nulla eu molestie. Duis eget elit nec erat volutpat convallis. Integer porttitor sed felis vel efficitur. Mauris elementum imperdiet enim, suscipit pharetra leo auctor ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-
-Curabitur consequat lacus eu dui tincidunt imperdiet. Cras ultrices iaculis nibh, non sagittis nunc rutrum nec. Phasellus ut pretium augue. Sed sit amet posuere sapien. Nulla placerat neque ac ullamcorper lobortis. Nunc ornare accumsan malesuada. Cras porta felis sapien, blandit tempor quam lacinia at. Etiam feugiat ligula porttitor consequat venenatis.
-
-Mauris blandit nibh mauris, in posuere sapien semper eu. Nulla in leo ut massa accumsan placerat fermentum nec dolor. Pellentesque mauris mauris, ultricies ut pretium vel, consectetur a risus. Nam a pulvinar mauris, ac vestibulum nisi. Phasellus maximus eros quis ipsum tempor venenatis. Ut eget mollis arcu, et convallis felis. Morbi tempor diam id lorem eleifend egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi consectetur ac neque vitae accumsan. Donec vehicula placerat arcu, sit amet efficitur odio lobortis eu. Duis eleifend, urna ut facilisis aliquet, arcu urna vulputate nisl, et sollicitudin diam justo a nibh. Praesent faucibus, felis eu molestie condimentum, est augue iaculis mi, vitae tempus purus mauris in felis. Pellentesque vel sollicitudin nibh, sed ultricies sapien.
-
-Pellentesque congue lacinia sapien et posuere. Phasellus nunc felis, porttitor non metus nec, gravida vulputate justo. Sed egestas pulvinar rutrum. Morbi id quam nulla. Donec venenatis, odio at rutrum molestie, mauris lorem laoreet arcu, eget cursus erat mi in velit. Aenean malesuada lacus vel volutpat suscipit. Pellentesque quis porttitor risus. Fusce neque mi, dictum fringilla tortor nec, viverra pellentesque quam. Ut consequat, nibh tincidunt dapibus pulvinar, purus purus condimentum orci, eu efficitur nisl arcu non lacus. Nunc tempor pretium orci vel suscipit. Donec a euismod mi.`
 
 template.innerHTML = `
 <style>
 #box {
     background-color: #C0C0C0;
-    width: 300px;
-    height: 500px;
     border: 1px solid black;
     border-top-width: 1px;
     border-right-width: 1px;
@@ -35,14 +14,12 @@ template.innerHTML = `
     overflow: hidden;
 }
 .container {
-    width: 400px;
-    height: 400px;
     display: flex;
     align-items: stretch;
 }
 </style>
 <div id="container">
-<div id="box">${lorem}</div>
+<div id="box"></div>
 </div>
 `
 
